d2df7d3cdcc5072fe584c136390b0205431243b5
Deepak <deepak.sisodiya@gmail.com>
2015-02-21 16:41:10 +0530

    done

--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,16 @@
 /**
  * Created by Deepak Sisodiya on 21/02/15.
  */
+
+app.controller("myNoteController", function($scope) {
+  $scope.message = "";
+  $scope.save = function() {
+    alert("Note Saved");
+  };
+  $scope.clear = function() {
+    $scope.message = "";
+  };
+  $scope.left = function() {
+    return 100 - $scope.message.length;
+  };
+})
--- src/module.js
+++ src/module.js
@@ -1,3 +1,5 @@
 /**
  * Created by Deepak Sisodiya on 21/02/15.
  */
+
+var app = angular.module("myNote", []);
